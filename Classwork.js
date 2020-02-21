05c6947fba87405b4fa9e9d37f9fde17908f2417
Takeallah <takeallahsrivera@icloud.com>
2020-02-21 14:19:57 -0600

    ClassworkPushOne

---
+++ Classwork.js
@@ -0,0 +1,44 @@
+
+/*## Problem 1
+Create a ```printNumbers``` function to 
+print integers from -25 to 20.
+*/
+
+//Problem 1 Solution//
+
+/*for (let k = -25;k <= 20; k++)
+{
+    console.log(k);
+
+}
+
+function printNumbers(k){
+    console.log(k);
+}
+
+*/
+
+//Problem 2//
+/*Create a function that’s passed a firstName 
+and lastName. Return lastName comma firstName.
+
+*/
+//Problem 2 Solution//
+/*let firstName = "";
+let lastName = "";
+
+function printNames(firstName, lastName){
+    return lastname, firstName
+}
+
+*/
+
+//Problem 3//
+/*
+Create a function in your program that 
+counts from 0 to [NUMBER]
+
+*/
+
+
+
